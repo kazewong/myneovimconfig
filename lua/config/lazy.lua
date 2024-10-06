b4b765c5d9dfd6c2b271834a37a667fa92735df2
Kaze Wong <kazewong.physics@gmail.com>
2024-10-06 14:14:47 -0400

    update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -58,6 +58,16 @@ require("lazy").setup({
 			"neoclide/coc.nvim",
 			branch = "release",
 		},
+		{
+			"nvim-neo-tree/neo-tree.nvim",
+			branch = "v3.x",
+			dependencies = {
+				"nvim-lua/plenary.nvim",
+				"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+				"MunifTanjim/nui.nvim",
+				-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+			},
+		},
 	},
 	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
