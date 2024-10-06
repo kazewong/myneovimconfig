228d7283950c078b1c67832ffa46dbe8c1706626
Kaze Wong <kazewong.physics@gmail.com>
2024-10-06 14:07:57 -0400

    update

---
+++ init.lua
@@ -0,0 +1,38 @@
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.spell = true
+vim.opt.spelllang = "en_us"
+require("config.lazy")
+require("Comment").setup()
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		-- Conform will run multiple formatters sequentially
+		python = { "isort", "black" },
+		-- You can customize some of the format options for the filetype (:help conform.format)
+		rust = { "rustfmt", lsp_format = "fallback" },
+		-- Conform will run the first available formatter
+		javascript = { "prettierd", "prettier", stop_after_first = true },
+		typst = { "typstyle" },
+	},
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 500,
+		lsp_format = "fallback",
+	},
+})
+
+require("wrapping").hard_wrap_mode()
+
+vim.cmd.colorscheme("catppuccin-mocha")
+
+local keyset = vim.keymap.set
+-- Autocomplete
+function _G.check_back_space()
+	local col = vim.fn.col(".") - 1
+	return col == 0 or vim.fn.getline("."):sub(col, col):match("%s") ~= nil
+end
+local opts = { silent = true, noremap = true, expr = true, replace_keycodes = false }
+keyset("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
+keyset("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
+keyset("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
